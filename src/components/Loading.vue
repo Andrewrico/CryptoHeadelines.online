8b523c1955a93d813e1a2094d055a1718aff0f4e
Andrew Rico <andrewric86@gmail.com>
2018-09-30 12:40:50 -0400

    Nine commit

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="fade">
-    <vue-loading :active="isLoading"></vue-loading>
+    <vue-loading class="loading" :active="isLoading"></vue-loading>
   </transition>
 </template>
 
@@ -32,17 +32,12 @@ export default {
   width: 100%;
   height: 100%;
   z-index: 5;
-  background-color: #0b1717;
-  background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAxIDEiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxyYWRpYWxHcmFkaWVudCBpZD0idnNnZyIgZ3JhZGllbnRVbml0cz0idXNlclNwYWNlT25Vc2UiIGN4PSIwJSIgY3k9IjAlIiByPSIxNDEuNDIxMzU2MjM3MzA5NSUiPjxzdG9wIHN0b3AtY29sb3I9IiMwMDJjMmMiIHN0b3Atb3BhY2l0eT0iMSIgb2Zmc2V0PSIwLjAxMTAwMDAwMDAwMDAwMDAwMSIvPjxzdG9wIHN0b3AtY29sb3I9IiMxNDA0MDQiIHN0b3Atb3BhY2l0eT0iMSIgb2Zmc2V0PSIwLjY1Nzk5OTk5OTk5OTk5OTkiLz48c3RvcCBzdG9wLWNvbG9yPSIjMDcyNjI2IiBzdG9wLW9wYWNpdHk9IjEiIG9mZnNldD0iMSIvPjwvcmFkaWFsR3JhZGllbnQ+PHJlY3QgeD0iLTUwIiB5PSItNTAiIHdpZHRoPSIxMDEiIGhlaWdodD0iMTAxIiBmaWxsPSJ1cmwoI3ZzZ2cpIiAvPjwvc3ZnPg==);
-  background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAxIDEiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxyYWRpYWxHcmFkaWVudCBpZD0idnNnZyIgZ3JhZGllbnRVbml0cz0idXNlclNwYWNlT25Vc2UiIGN4PSIwJSIgY3k9IjAlIiByPSIxNDEuNDIxMzU2MjM3MzA5NSUiPjxzdG9wIHN0b3AtY29sb3I9IiMwMDJjMmMiIHN0b3Atb3BhY2l0eT0iMSIgb2Zmc2V0PSIwLjAxMTAwMDAwMDAwMDAwMDAwMSIvPjxzdG9wIHN0b3AtY29sb3I9IiMxNDA0MDQiIHN0b3Atb3BhY2l0eT0iMSIgb2Zmc2V0PSIwLjY1Nzk5OTk5OTk5OTk5OTkiLz48c3RvcCBzdG9wLWNvbG9yPSIjMDcyNjI2IiBzdG9wLW9wYWNpdHk9IjEiIG9mZnNldD0iMSIvPjwvcmFkaWFsR3JhZGllbnQ+PHJlY3QgeD0iLTUwIiB5PSItNTAiIHdpZHRoPSIxMDEiIGhlaWdodD0iMTAxIiBmaWxsPSJ1cmwoI3ZzZ2cpIiAvPjwvc3ZnPg==);
-  background-image: -webkit-radial-gradient(left top, ellipse farthest-corner, rgb(0, 44, 44) 1.1%, rgb(20, 4, 4) 65.8%, rgb(7, 38, 38) 100%);
-  background-image: radial-gradient(ellipse farthest-corner at left top, rgb(0, 44, 44) 1.1%, rgb(20, 4, 4) 65.8%, rgb(7, 38, 38) 100%);
-  background-image: -ms-radial-gradient(left top, ellipse farthest-corner, rgb(0, 44, 44) 1.1%, rgb(20, 4, 4) 65.8%, rgb(7, 38, 38) 100%);
 }
 
 svg {
+  color:red;
   font-size: 2em;
-  color: var(--c-primary);
+  /* color: var(--c-primary); */
   margin-bottom: 20px;
 }
 
--- src/lambda/feeds.js
+++ src/lambda/feeds.js
@@ -1,17 +1,23 @@
 import Parser from 'rss-parser'
 
 const FEEDS = [
-  'https://blog.coinspectator.com/feed/',
+
+  // 'https://ethereumworldnews.com/feed/',
+  
   'https://www.coindesk.com/feed/',
   'https://www.bitcoin.com/feed/',
   'https://cointelegraph.com/feed',
   'https://www.ccn.com/feed/',
   'https://nulltx.com/feed/',
   'https://toshitimes.com/feed/',
+  'https://blog.kraken.com/feed/',
+  'https://cryptopotato.com/feed/',
+  'https://blog.cex.io/cryptonews/feed',
   'https://bitcoinist.com/feed/',
   'https://bitcoinmagazine.com/feed/',
   'https://www.newsbtc.com/feed/',
   'https://99bitcoins.com/feed/',
+  'https://blog.coinspectator.com/feed/',
   
 ]
 
